3711606965575ae69beb76c45c6547cd0d93762a
Martin_cnt <martin.cuinet02@edu.univ-fcomte.fr.com>
2025-01-23 16:08:48 +0100

    jeans

--- sae_sql.sql
+++ sae_sql.sql
@@ -88,3 +88,39 @@ INSERT INTO etat VALUES(null, "en attente") ;
 INSERT INTO etat VALUES(null, "expédié") ;
 INSERT INTO etat VALUES(null, "validé") ;
 INSERT INTO etat VALUES(null, "confirmé") ;
+
+INSERT INTO taille(id_taille, nom_taille) VALUES
+(NULL, 'W29'),
+(NULL, 'W30'),
+(NULL, 'W32'),
+(NULL, 'W34'),
+(NULL, 'W38');
+
+INSERT INTO coupe_jean(id_coupe_jean, nom_coupe) VALUES
+(NULL, 'slim'),
+(NULL, 'droit'),
+(NULL, 'large'),
+(NULL, 'ajuste');
+
+INSERT INTO jean(id_jean, nom_jean, prix_jean, matiere, couleur, description, marque, fournisseur, photo, taille_id, coupe_jean_id) VALUES
+(NULL, 'jean Xantia ', '45', 'coton','blanc', 'Jean ajuste blanc', 'Levis', 'Herpi','jean_blanc_ajuste.jpg',NULL, NULL),
+(NULL, 'jean Picasso ', '50', 'polyester','blanc', 'Jean droit blanc', 'Bershka', 'Herpi','jean_blanc_droit.jpg',NULL, NULL),
+(NULL, 'jean Santiago ', '70', 'materiaux recyclés','blanc', 'Jean large blanc', 'Jack and Jones', 'BZB','jean_blanc_large.jpg',NULL, NULL),
+(NULL, 'jean Mirabella ', '50', 'polyester','blanc', 'Jean large blanc', 'Bershka', 'Herpi','jean_blanc_large2.jpg',NULL, NULL),
+(NULL, 'jean Starlight ', '60', 'coton','blanc', 'Jean slim blanc', 'Calvin Klein', 'Officine Générale','jean_blanc_slim.jpg',NULL, NULL),
+(NULL, 'jean Summer ', '70', 'coton','blanc', 'Jean slim blanc', 'Levis', 'BZB','jean_blanc_slim2.jpg',NULL, NULL),
+(NULL, 'jean Winter ', '45', 'coton','bleu', 'Jean ajuste bleu', 'Jack and Jones', 'Officine Générale','jean_bleu_ajuste.jpg',NULL, NULL),
+(NULL, 'jean Flower ', '50', 'materiaux recyclés','bleu', 'Jean ajuste bleu', 'Calvin Klein', 'BZB','jean_bleu_ajuste2.jpg',NULL, NULL),
+(NULL, 'jean OG ', '60', 'polyester','bleu', 'Jean droit bleu', 'Bershka', 'BZB','jean_bleu_droit.jpg',NULL, NULL),
+(NULL, 'jean Simple ', '40', 'coton','bleu', 'Jean droit bleu', 'Levis', 'Officine Générale','jean_bleu_droit2.jpg',NULL, NULL),
+(NULL, 'jean Crazy ', '45', 'polyester','bleu', 'Jean large bleu', 'Calvin Klein', 'Herpi','jean_bleu_large.jpg',NULL, NULL),
+(NULL, 'jean Brazil ', '70', 'polyester','bleu', 'Jean large bleu', 'Jack and Jones', 'BZB','jean_bleu_large2.jpg',NULL, NULL),
+(NULL, 'jean Tropical ', '50', 'coton','bleu', 'Jean large bleu', 'Bershka', 'Officine Générale','jean_bleu_large3.jpg',NULL, NULL),
+(NULL, 'jean Ecolo ', '60', 'materiaux recyclés','bleu', 'Jean slim bleu', 'Levis', 'Herpi','jean_bleu_slim.jpg',NULL, NULL),
+(NULL, 'jean Mer ', '40', 'coton','bleu', 'Jean slim bleu', 'Jack and Jones', 'Herpi','jean_bleu_slim2.jpg',NULL, NULL),
+(NULL, 'jean Black ', '40', 'polyester','noir', 'Jean ajuste noir', 'Jack and Jones', 'Officine Générale','jean_noir_ajuste.jpg',NULL, NULL),
+(NULL, 'jean Noche ', '70', 'coton','noir', 'Jean ajuste noir', 'Calvin Klein', 'BZB','jean_noir_ajuste2.jpg',NULL, NULL),
+(NULL, 'jean Classico ', '50', 'coton','noir', 'Jean ajuste noir', 'Levis', 'Herpi','jean_noir_ajuste3.jpg',NULL, NULL),
+(NULL, 'jean Recycle ', '40', 'materiaux recyclés','noir', 'Jean droit noir', 'Bershka', 'BZB','jean_noir_droit.jpg',NULL, NULL),
+(NULL, 'jean Michou ', '70', 'polyester','noir', 'Jean droit noir', 'Calvin Klein', 'Officine Générale','jean_noir_droit2.jpg',NULL, NULL),
+(NULL, 'jean Goat ', '45', 'coton','noir', 'Jean slim noir', 'Bershka', 'BZB','jean_noir_slim.jpg',NULL, NULL);
