367e9e130d1a0ce350fb3b6a8e333df2f1e024fb
EthanCHEVRAND <ethan.chevrand@edu.univ-fcomte.fr>
2025-01-24 09:33:58 +0100

    Update sae_sql.sql

--- sae_sql.sql
+++ sae_sql.sql
@@ -25,8 +25,9 @@ CREATE TABLE utilisateur(
    login VARCHAR(50),
    email VARCHAR(50),
    nom VARCHAR(50),
-   password VARCHAR(50),
+   password VARCHAR(255),
    role VARCHAR(50),
+   est_actif VARCHAR(1),
    PRIMARY KEY(id_utilisateur)
 );
 CREATE TABLE etat(
@@ -124,4 +125,14 @@ INSERT INTO jean(id_jean, nom_jean, prix_jean, matiere, couleur, description, ma
 (NULL, 'jean Michou ', '70', 'polyester','noir', 'Jean droit noir', 'Calvin Klein', 'Officine Générale','jean_noir_droit2.jpg',NULL, NULL),
 (NULL, 'jean Goat ', '45', 'coton','noir', 'Jean slim noir', 'Bershka', 'BZB','jean_noir_slim.jpg',NULL, NULL);
 
-INSERT INTO utilisateur (login, email, nom, password, role) VALUES ("client", "client@test.fr", "Sacré Client", "client", "user");
+INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) VALUES
+(1,'admin','admin@admin.fr',
+    'scrypt:32768:8:1$irSP6dJEjy1yXof2$56295be51bb989f467598b63ba6022405139656d6609df8a71768d42738995a21605c9acbac42058790d30fd3adaaec56df272d24bed8385e66229c81e71a4f4',
+    'ROLE_admin','admin','1'),
+(2,'client','client@client.fr',
+    'scrypt:32768:8:1$iFP1d8bdBmhW6Sgc$7950bf6d2336d6c9387fb610ddaec958469d42003fdff6f8cf5a39cf37301195d2e5cad195e6f588b3644d2a9116fa1636eb400b0cb5537603035d9016c15910',
+    'ROLE_client','client','1'),
+(3,'client2','client2@client2.fr',
+    'scrypt:32768:8:1$l3UTNxiLZGuBKGkg$ae3af0d19f0d16d4a495aa633a1cd31ac5ae18f98a06ace037c0f4fb228ed86a2b6abc64262316d0dac936eb72a67ae82cd4d4e4847ee0fb0b19686ee31194b3',
+    'ROLE_client','client2','1');
+
